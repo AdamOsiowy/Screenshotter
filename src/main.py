7b149a64fce998d181fa43ab5a998a9e854efd42
Adam Osiowy <adaosi@st.amu.edu.pl>
2021-04-03 22:28:42 +0200

    add MainWindow class that inherits from Ui class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+import sys
+from PyQt5 import QtWidgets
+from user_interface import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+
 def main():
-    print("hello")
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
